e214d2f86d4e4ab41db1a9e09dd0670bc23054a4
Robert04 <bobby.toth@gmail.com>
2023-10-06 15:25:08 -0500

    added functionality

--- Developers1/main.py
+++ Developers1/main.py
@@ -1,9 +1,13 @@
 incompleteTasks = []
 completeTasks = []
 
+def showTasks():
+    print(f"To-Do List: {incompleteTasks}")
+    print(f"Completed Tasks: {completeTasks}")
 
 def addTasks(task):
     incompleteTasks.append(task)
+    print(f"Tasks successfully added! Here are your current tasks: {showTasks()}")
 
 def completeTasks(task):
     completeTasks.append(task)
