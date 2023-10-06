5ce86d3742db469e66df78d7f4f16c98d69bb6a7
Robert04 <bobby.toth@gmail.com>
2023-10-06 16:22:33 -0500

    MVP

--- Developers1/main.py
+++ Developers1/main.py
@@ -8,23 +8,91 @@ def greeting():
     print("3: View Completed Tasks \n")
     showTasks()
 
-    choice = input("test")
+    choice = input("")
+
+    if choice == "1":
+        addTasks()
+    elif choice == "2":
+        deleteTasks()
+    elif choice == "3":
+        showTasks()
+    else:
+        print("Invalid Input")
 
 
 def showTasks():
     print(f"To-Do List: {incompleteTasks}")
     print(f"Completed Tasks: {completeTasks}")
 
-def addTasks(task):
+
+
+def addTasks():
+    task = input("Enter Task: ")
     incompleteTasks.append(task)
-    return(print(f"Tasks successfully added! Here are your current tasks: {incompleteTasks}"))
+    print(f"Tasks successfully added! Here are your current tasks: {incompleteTasks}")
+
+    print("Select an option: ")
+    print("1: Add Tasks")
+    print("2: Delete Tasks")
+    print("3: View Completed Tasks \n")
+    showTasks()
+
+    choice = input("")
+
+    if choice == "1":
+        addTasks()
+    elif choice == "2":
+        deleteTasks()
+    elif choice == "3":
+        showTasks()
+    else:
+        print("Invalid Input")
+
     
-
-def completeTasks(task):
+def completeTasks():
+    task = input("Which task is completed? ")
+    print("Congrats, task completed")
     completeTasks.append(task)
-
-def deleteTasks(task):
     incompleteTasks.remove(task)
+
+    print("Select an option: ")
+    print("1: Add Tasks")
+    print("2: Delete Tasks")
+    print("3: View Completed Tasks \n")
+    showTasks()
+
+    choice = input("")
+
+    if choice == "1":
+        addTasks()
+    elif choice == "2":
+        deleteTasks()
+    elif choice == "3":
+        showTasks()
+    else:
+        print("Invalid Input")
+
+
+def deleteTasks():
+    task = input("Which tasks do you want to delete? ")
+    incompleteTasks.remove(task)
+
+    print("Select an option: ")
+    print("1: Add Tasks")
+    print("2: Delete Tasks")
+    print("3: View Completed Tasks \n")
+    showTasks()
+
+    choice = input("")
+
+    if choice == "1":
+        addTasks()
+    elif choice == "2":
+        deleteTasks()
+    elif choice == "3":
+        showTasks()
+    else:
+        print("Invalid Input")
 
 greeting()
 #addTasks("testing")
