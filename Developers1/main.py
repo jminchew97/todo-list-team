e7e7a7a51777596890186c5cad36918d01d5988b
Robert04 <bobby.toth@gmail.com>
2023-10-06 15:51:10 -0500

    Almost there

--- Developers1/main.py
+++ Developers1/main.py
@@ -1,13 +1,24 @@
 incompleteTasks = []
 completeTasks = []
 
+def greeting():
+    print("Welcome to your task manager: Select an option: ")
+    print("1: Add Tasks")
+    print("2: Delete Tasks")
+    print("3: View Completed Tasks \n")
+    showTasks()
+
+    choice = input("test")
+
+
 def showTasks():
     print(f"To-Do List: {incompleteTasks}")
     print(f"Completed Tasks: {completeTasks}")
 
 def addTasks(task):
     incompleteTasks.append(task)
-    print(f"Tasks successfully added! Here are your current tasks: {showTasks()}")
+    return(print(f"Tasks successfully added! Here are your current tasks: {incompleteTasks}"))
+    
 
 def completeTasks(task):
     completeTasks.append(task)
@@ -15,4 +26,5 @@ def completeTasks(task):
 def deleteTasks(task):
     incompleteTasks.remove(task)
 
-print(addTasks(task))
+greeting()
+#addTasks("testing")
